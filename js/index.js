b4731b3fcb8271880228f135d053a4df0c2d0ac7
unknown <sanodzegg@gmail.com>
2022-03-04 21:15:55 +0400

    fixed some validations and added language add functional

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ const data = {
     token: TOKEN,
 }
 
+let skill_data = {};
+
 document.addEventListener("DOMContentLoaded", () => {
     // declaration
     const dotbox = document.querySelectorAll('.page-dot');
@@ -15,9 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const textareas = document.querySelectorAll('textarea');
     const formbox = document.querySelectorAll('.form-box');
     const displaybox = document.querySelectorAll('.display-wrapper');
+    const languageAddBtn = document.getElementById('toLangList');
+
     let count = 0;
     let err = {};
-
+    let skill_arr = [0, 1];
     let selected = false;
 
     document.getElementById('start').onclick = () => {
@@ -35,9 +39,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     getSkills(selectors[0]);
-    document.getElementById('toLangList').addEventListener('click', function(){
-        if(null){} //fix
+
+    languageAddBtn.addEventListener('click', function(){
+        document.querySelector('.langlist-wrapper').innerHTML += 
+        `
+        <div class="langlist-box">
+            <span id="langName">${skill_arr[1]}</span>
+            <span id="expYears">years of experience: ${skill_arr[0]}</span>
+            <div class="removeLang">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10C19.9939 15.5203 15.5203 19.9939 10 20ZM2 10.172C2.04732 14.5732 5.64111 18.1095 10.0425 18.086C14.444 18.0622 17.9995 14.4875 17.9995 10.086C17.9995 5.68451 14.444 2.10977 10.0425 2.086C5.64111 2.06246 2.04732 5.59876 2 10V10.172ZM15 11H5V9H15V11Z" fill="#EB3535"/>
+                </svg>                                
+            </div>
+        </div>
+        `
+        const removers = document.querySelectorAll('.removeLang');
+        removers.forEach(e => {
+            e.addEventListener('click', function(){
+                e.parentNode.remove();
+            })
+        })
     });
+
+    
     // validation
     inputs.forEach(e => {
         err.textval = false;
@@ -86,7 +110,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 if(e.value.length == 0) {
                     err.exp = false;
                 } else {
-                   delete err.exp;
+                    skill_arr.splice(0, 1, e.value);
+                    delete err.exp;
                 }
             }
             if(Object.keys(err).length == 0) {
@@ -100,13 +125,29 @@ document.addEventListener("DOMContentLoaded", () => {
             selectors.forEach(e => {
                 if(e.offsetParent !== null && selected == false) {
                     err.selector = false;
+                } else {
+                    delete err.selector;
                 }
                 e.onchange = (c) => {
+                    console.log(err);
                     selected = true;
-                    delete err.selector
+                    Object.values(skill_data).find(key => {
+                        if(key.title === c.target.value) {
+                            skill_arr.splice(1, 1, c.target.value);
+                        }
+                    });
                 }
             });
-            console.log(data);
+            console.log(err);
+            // console.log(skill_arr);
+
+            if(typeof(skill_arr[1]) === 'string' && Object.keys(err).length == 0) {
+                languageAddBtn.style.pointerEvents = 'all';
+                languageAddBtn.style.cursor = 'pointer';
+            } else {
+                languageAddBtn.style.pointerEvents = 'none';
+                languageAddBtn.style.cursor = 'unset';
+            }
         }
     });
     dotbox.forEach((e, i) => {
@@ -147,8 +188,9 @@ const getSkills = async (node) => {
         }
     });
     let data = await res.json();
+    skill_data = Object.assign({}, data)
     data.forEach(e => {
-        node.innerHTML += 
+        node.innerHTML +=
         `
             <option value="${e.title}">${e.title}</option>
         `
