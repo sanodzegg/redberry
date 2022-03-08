bf2a442c3cee3435ddc94ae4a2098b7a9c015884
sanodzegg <sanodzegg@gmail.com>
2022-03-08 04:33:31 +0400

    updated validations and fixed skill section in main data object

--- js/index.js
+++ js/index.js
@@ -19,6 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const displaybox = document.querySelectorAll('.display-wrapper');
     const languageAddBtn = document.getElementById('toLangList');
 
+    const submit = document.getElementById('submitButton');
+    const back = document.getElementById('goBackBtn');
+
     // coordination validation
     const coordinationVal = document.querySelectorAll('.coordination');
     // skills validation
@@ -40,16 +43,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let count = 0;
     let skill_arr = [0, 1];
     // this is for data object 
-    let skillid;
-    let skill = {};
     let skills = [];
 
     document.getElementById('start').onclick = () => {
         document.body.style.background = 'unset';
         document.querySelector('.home-view').classList.add('hidden');
         document.querySelector('.form-section').classList.remove('hidden');
-        formbox[1].classList.remove('hidden');
-        formbox[1].classList.add('available');
+        formbox[0].classList.remove('hidden');
+        formbox[0].classList.add('available');
         displaybox[0].classList.remove('hidden');
     }
 
@@ -81,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
         `
         <div class="langlist-box">
             <span class="langName" id="langName">${skill_arr[1]}</span>
-            <span id="expYears">years of experience: ${skill_arr[0]}</span>
+            <span class="experience">years of experience: ${skill_arr[0]}</span>
             <div class="removeLang">
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10C19.9939 15.5203 15.5203 19.9939 10 20ZM2 10.172C2.04732 14.5732 5.64111 18.1095 10.0425 18.086C14.444 18.0622 17.9995 14.4875 17.9995 10.086C17.9995 5.68451 14.444 2.10977 10.0425 2.086C5.64111 2.06246 2.04732 5.59876 2 10V10.172ZM15 11H5V9H15V11Z" fill="#EB3535"/>
@@ -90,14 +91,9 @@ document.addEventListener("DOMContentLoaded", () => {
         </div>
         `
         delete skills_err.exppoints;
-        // pushing skill data into main object
-        skill.id = skillid;
-        skill.experience = parseInt(skill_arr[0]);    
-        skills.push(skill);
-        skill = {};
-        data.skills = [skills];
+
         // inner declarations for added skills
-        const removers = document.querySelectorAll('.removeLang');
+        let removers = document.querySelectorAll('.removeLang');
         const addedSkills = document.querySelectorAll('.langName');
         const options = document.querySelectorAll('option');
 
@@ -110,34 +106,33 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         }
         removers.forEach((e, i) => {
-            e.addEventListener('click', function(){
+            e.addEventListener('click', function() {
                 for(let i = 0; i < options.length; i++) {
                     if(e.parentNode.childNodes[1].innerText == options[i].innerText) {
                         options[i].removeAttribute('disabled');
                         languageAddBtn.style.pointerEvents = 'all';
                     }
                 }
-                for(let e = 0; e < document.querySelectorAll('.removeLang').length; e++) {
-                    console.log(document.querySelectorAll('.removeLang')[e]);
-                }
-                skills.splice(i, 1, "");
+                skills.splice(i, 1);
                 skills = skills.filter((el) => {
-                    return el != "";
+                    return el != null;
                 });
-                data.skills = [skills];
+                // data.skills = [skills];
                 e.parentNode.remove();
+                // removers = document.querySelectorAll('.removeLang');
+                
                 if(document.querySelectorAll('.removeLang').length < 1) {
                     skills_err.exppoints = false;
-                    data.skills = {};
                     validator(skills_err, 1);
                 }
             });
-            console.log(data);
         });
         validator(skills_err, 1);
     });
 
-    
+    let error_desc = document.createElement('span');
+    error_desc.classList.add('err-text');
+
     // validation
     coordinationVal.forEach(e => {
         coord_err.textval = false;
@@ -146,15 +141,36 @@ document.addEventListener("DOMContentLoaded", () => {
             if(e.id == 'fname' || e.id == 'lname'){
                 if(e.value.length == 0) {
                     coord_err.textval = false;
-                    e.style.border = '1px #FE3B1F solid';
+                    if(e.id == 'fname') {
+                        e.style.border = '1px #FE3B1F solid';
+                        error_desc.innerText = '* first name is required';
+                        insertAfter(e, error_desc);
+                    } else if (e.id == 'lname') {
+                        e.style.border = '1px #FE3B1F solid';
+                        error_desc.innerText = '* last name is required';
+                        insertAfter(e, error_desc);
+                    }
                 } else if (e.value.length <= 2) {
                     coord_err.textval = false;
+                    if(e.id == 'fname') {
+                        e.style.border = '1px #FE3B1F solid';
+                        error_desc.innerText = '* first name should include 3 or more characters';
+                        insertAfter(e, error_desc);
+                    } else if (e.id == 'lname') {
+                        e.style.border = '1px #FE3B1F solid';
+                        error_desc.innerText = '* last name should include 3 or more characters';
+                        insertAfter(e, error_desc);
+                    }
                 } else {
                     delete coord_err.textval;
                     if(e.id == 'fname') {
                         data.first_name = e.value;
+                        e.style.border = '';
+                        error_desc.remove();
                     } else if (e.id == 'lname') {
                         data.last_name = e.value;
+                        e.style.border = '';
+                        error_desc.remove();
                     }
                 }
             }
@@ -162,24 +178,43 @@ document.addEventListener("DOMContentLoaded", () => {
                 if(mail.test(e.value)) {
                     delete coord_err.email;
                     data.email = e.value;
+                    e.style.border = '';
+                    error_desc.remove();
                 } else if (e.value.length == 0) {
                     coord_err.email = false;
                     e.style.border = '1px #FE3B1F solid';
+                    error_desc.innerText = '* email is required';
+                    insertAfter(e, error_desc);
                 } else {
-                    delete coord_err.email;
-                    data.email = e.value;
+                    coord_err.email = false;
+                    e.style.border = '1px #FE3B1F solid';
+                    error_desc.innerText = '* email is incorrect';
+                    insertAfter(e, error_desc);
                 }
             }
             if(e.id == 'number') {
+                let body = document.getElementById('number');
                 if(e.value.length == 0) {
                     delete coord_err.number;
-                    e.style.border = '1px #525557 solid';
+                    e.style.border = '';
                 } else if (e.value.length > 1 && e.value.length != 11) {
                     coord_err.number = false;
+                    e.style.border = '1px #FE3B1F solid';
+                    error_desc.innerText = '* number is incorrect';
+                    insertAfter(body, error_desc);
                 } else if(e.value[0] != '5') {
                     coord_err.number = false;
+                    e.style.border = '1px #FE3B1F solid';
+                    error_desc.innerText = '* number is incorrect';
+                    insertAfter(body, error_desc);
+                } else if (parseInt(e.value.replace(/-/g, '')).toString().length < 9) {
+                    e.style.border = '1px #FE3B1F solid';
+                    error_desc.innerText = '* number is incorrect';
+                    insertAfter(body, error_desc);
                 } else {
-                   delete coord_err.number;
+                    delete coord_err.number;
+                    e.style.border = '';
+                    error_desc.remove();
                 }
             }
             validator(coord_err, 0)
@@ -204,9 +239,11 @@ document.addEventListener("DOMContentLoaded", () => {
             if(e.id == 'exp') {
                 if(e.value.length == 0) {
                     skills_err.values.exp = false;
+                    e.style.border = '1px #FE3B1F solid';
                 } else {
                     skill_arr.splice(0, 1, e.value);
                     delete skills_err.values.exp;
+                    e.style.border = '';
                 }
             }
             if(document.querySelectorAll('.removeLang').length >= 1) {
@@ -249,10 +286,15 @@ document.addEventListener("DOMContentLoaded", () => {
                             if(`${Date.parse(e.target.value)}` !== `NaN`) {
                                 delete covid_err.coviddate;
                                 data.had_covid_at = `${e.target.value}`;
+                                dateInputs[0].style.border = '';
+                                error_desc.remove();
                             } else {
                                 covid_err.coviddate = false;
                                 delete data.had_covid_at;
                                 // do your validation magic
+                                dateInputs[0].style.border = '1px #FE3B1F solid';
+                                error_desc.innerText = '* date is required';
+                                insertAfter(dateInputs[0], error_desc);
                             }
                             validator(covid_err, 0)
                         }
@@ -274,10 +316,15 @@ document.addEventListener("DOMContentLoaded", () => {
                             if(`${Date.parse(e.target.value)}` != `NaN`) {
                                 delete covid_err.vaccdate;
                                 data.vaccinated_at = `${e.target.value}`;
+                                dateInputs[1].style.border = '';
+                                error_desc.remove();
                             } else {
                                 covid_err.vaccdate = false;
                                 delete data.vaccinated_at;
                                 // do your validation magic
+                                dateInputs[1].style.border = '1px #FE3B1F solid';
+                                error_desc.innerText = '* date is required';
+                                insertAfter(dateInputs[1], error_desc);
                             }
                             validator(covid_err, 0)
                         }
@@ -303,11 +350,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 if(e.value == 'yes') {
                     delete devtalks_err.attend;
                     data.will_organize_devtalk = true;
-                } else if(e.value == 'no') {
+                    document.querySelectorAll('.radio-box')[6].classList.remove('hidden');
+                } else {
                     delete devtalks_err.attend;
                     data.will_organize_devtalk = false;
-                } else {
-                    devtalks_err.attend = false;
+                    document.querySelectorAll('.radio-box')[6].classList.add('hidden');
                 }
                 validator(devtalks_err, 0);
             }
@@ -317,17 +364,27 @@ document.addEventListener("DOMContentLoaded", () => {
                     if(e.value.length > 0) {
                         delete devtalks_err.speaker;
                         data.devtalk_topic = `${e.value}`;
+                        e.style.border = '';
+                        error_desc.remove();
                     } else {
                         devtalks_err.speaker = false;
-                        delete data.devtalk_topic;
+                        data.devtalk_topic = 'I would ...';
+                        e.style.border = '1px #FE3B1F solid';
+                        error_desc.innerText = '* this field is required';
+                        insertAfter(e, error_desc);
                     }
                 } else {
                     if(e.value.length > 0) {
                         delete devtalks_err.special;
                         data.something_special = `${e.value}`;
+                        e.style.border = '';
+                        error_desc.remove();
                     } else {
                         devtalks_err.special = false;
-                        delete data.devtalk_topic;
+                        data.devtalk_topic = 'I am special!';
+                        e.style.border = '1px #FE3B1F solid';
+                        error_desc.innerText = '* this field is required';
+                        insertAfter(e, error_desc);
                     }
                 }
                 validator(devtalks_err, 0);
@@ -351,24 +408,66 @@ document.addEventListener("DOMContentLoaded", () => {
                     document.querySelector('.form-section').classList.add('hidden');
                     document.querySelector('.submit-section').classList.remove('hidden');
                     document.body.style.background = '$primary-bg';
-                    console.log(data);
                 }
             }
         });
     });
-    // document.getElementById('prev').addEventListener('click', function(){
-    //     if(count > 0) {
-    //         count--;
-    //         dotbox[count].classList.add('active');
-    //     }
-    // });
-    // document.getElementById('next').addEventListener('click', function(){
-    //     if(count < 4) {
-    //         count++;
-    //         dotbox[count].classList.add('active');
-    //     }
-    // });
+    document.getElementById('prev').addEventListener('click', function(){
+        if(count > 0) {
+            count--;
+            if(dotbox[count].classList.contains('available')) {
+                dotbox[count].classList.add('active');
+                for(let i = 0; i < formbox.length; i++) {
+                    formbox[i].classList.add('hidden');
+                    displaybox[i].classList.add('hidden');
+                }
+                formbox[count].classList.remove('hidden');
+                displaybox[count].classList.remove('hidden');
+            } else {
+                count++;
+            }
+        }
+    });
+    document.getElementById('next').addEventListener('click', function(){
+        if(count < 4) {
+            count++;
+            if(dotbox[count].classList.contains('available')) {
+                dotbox[count].classList.add('active');
+                for(let i = 0; i < formbox.length; i++) {
+                    formbox[i].classList.add('hidden');
+                    displaybox[i].classList.add('hidden');
+                }
+                formbox[count].classList.remove('hidden');
+                displaybox[count].classList.remove('hidden');
+            } else {
+                count--;
+            }
+        }
+    });
+    submit.addEventListener('click', () => {
+        let names = document.querySelectorAll('.langName');
+        let exp = document.querySelectorAll('.experience');
+        // pushing skill data into main object
+        let skills = [];
+        for(let i = 0; i < names.length; i++) {
+            Object.values(skill_data).find(key => {
+                if(key.title === names[i].innerText) {
+                    skills.push({id: key.id, experience: parseInt(exp[i].innerText.charAt(exp[i].innerText.length - 1))});
+                }
+            });
+        }
+        data.skills = [skills];
+        console.log(data);
+    });
+    back.addEventListener('click', () => {
+        document.querySelector('.submit-section').classList.add('hidden');
+        document.querySelector('.form-section').classList.remove('hidden');
+    });
 });
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
 
 const getSkills = async (node) => {
     let res = await fetch('https://bootcamp-2022.devtest.ge/api/skills', {
